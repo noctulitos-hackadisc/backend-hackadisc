54339fcaa74b1c4f29d7bb8522fe4dd98000a730
ExequielGL <exequiel.gonzales@gmail.com>
2024-06-22 03:56:32 -0400

    feat: worker name in get worker's evaluations endpoint

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -36,6 +36,8 @@ class EvaluationController extends Controller
         $evaluations = Evaluation::where('worker_id', $id)->get();
 
         $formattedEvaluations = $evaluations->map(function ($evaluation) {
+            $evaluation->worker_name = $evaluation->worker->name;
+            unset($evaluation['worker']);
             $evaluation->date = Carbon::parse($evaluation->date)->format('d/m/Y');
             return $evaluation;
         });
